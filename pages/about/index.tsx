855adcb40c061f4b40a8d8df98bd6f1e9291311d
CasianLW <ciorbacasian@gmail.com>
2023-01-26 15:07:12 +0100

    Init base projet Portfolio personnel

---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+// import ContectComponent from '@/components/contact-components/formComponent'
+import { NextPage } from 'next'
+
+const About: NextPage = () => (
+  <>
+    <h1>About</h1>
+  </>
+)
+
+export default About
---
+++ pages/contact/index.tsx
@@ -0,0 +1,11 @@
+import ContectComponent from '@/components/contact-components/formComponent'
+import { NextPage } from 'next'
+
+const Contact: NextPage = () => (
+  <>
+    <h1>Contactez-moi</h1>
+    <ContectComponent />
+  </>
+)
+
+export default Contact
