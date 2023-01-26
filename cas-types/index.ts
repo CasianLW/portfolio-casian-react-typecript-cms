27353d2f43dd257a7934774a9245ecdb0d198ef3
CasianLW <ciorbacasian@gmail.com>
2023-01-26 17:59:27 +0100

    ajout type interface project

--- cas-types/index.ts
+++ cas-types/index.ts
@@ -2,3 +2,15 @@ export interface Seo {
   title: string
   description: string
 }
+
+export interface Project {
+  title: string
+  seo: { title: string; description: string }
+  slug: string
+  methods: string[]
+  primaryImage: string
+  secondaryImage: string
+  description: string
+  liens: string[]
+  order: number
+}
