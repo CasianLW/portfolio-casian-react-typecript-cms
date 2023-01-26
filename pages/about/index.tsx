414321584524d42b55fb99db01b76f915a6103e5
CasianLW <ciorbacasian@gmail.com>
2023-01-26 16:28:26 +0100

    Utils necessaires au fonctionnement du SEO ainsi que la partie content qui influence les pages

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,22 @@
 // import ContectComponent from '@/components/contact-components/formComponent'
-import { NextPage } from 'next'
+import { Seo } from '@/cas-types'
+import SeoComponent from '@/components/shared/seo-component'
+import { getPageSeoBySlug } from '@/utils/content-api'
+import { GetStaticProps, NextPage } from 'next'
 
-const About: NextPage = () => (
+interface Props {
+  seo: Seo
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const seo = getPageSeoBySlug('about')
+  return {
+    props: { seo },
+  }
+}
+const About: NextPage<Props> = ({ seo }) => (
   <>
+    <SeoComponent seo={seo} />
     <h1>About</h1>
   </>
 )
