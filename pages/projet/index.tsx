2db1e2d918a93b0160e2dbd1616a916580a1f7bc
CasianLW <ciorbcasian@gmail.com>
2023-01-26 18:01:28 +0100

    Debut developpemet page projet et son design

--- pages/projet/index.tsx
+++ pages/projet/index.tsx
@@ -1,8 +1,22 @@
-// import ContectComponent from '@/components/contact-components/formComponent'
-import { NextPage } from 'next'
+import { Seo } from '@/cas-types'
+import SeoComponent from '@/components/shared/seo-component'
+import { getPageSeoBySlug } from '@/utils/content-api'
+import { GetStaticProps, NextPage } from 'next'
 
-const Projet: NextPage = () => (
+interface Props {
+  seo: Seo
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const seo = getPageSeoBySlug('projet')
+  return {
+    props: { seo },
+  }
+}
+const Projet: NextPage<Props> = ({ seo }) => (
   <>
+    <SeoComponent seo={seo} />
+
     <h1>Projet</h1>
   </>
 )
