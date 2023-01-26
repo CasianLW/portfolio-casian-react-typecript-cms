12585016b75e1bb7764b13a2c798643128142db8
CasianLW <ciorbcasian@gmail.com>
2023-01-26 15:59:17 +0100

    Ajout des couleurs / gradients et autres elements pour tailwind config selon la maquette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,57 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        fraunces: ['Fraunces', 'sans-serif'],
+        secondary: ['Minerva Modern', 'sans-serif'],
+      },
+      colors: {
+        'cas-black': {
+          400: '#262626',
+          500: '#1c1c1c',
+          600: '#151515',
+        },
+        'cas-white': {
+          100: '#ffffff',
+          300: '#949494',
+        },
+        'cas-gradient': {
+          purple: '#6c52ee',
+          orangeLight: '#f5d8a4',
+          orangeDark: '#b08676',
+          blue: '#a8c5de',
+          rose: '#c79dde',
+        },
+        'cas-gradient-purple': '#00B3D2',
+        'cas-gradient-orange': '#2A3B7C',
+        'cwr-yellow': '#F8AE00',
+        'cwr-green': '#009642',
+        'cwr-purple': '#9065A8',
+        'cwr-pink': '#EB608E',
+      },
+      boxShadow: {
+        'btn-dark': '2px 2px 0px 1px #000000',
+        'btn-light': '2px 2px 0px 1px #ffffff',
+        'faq-detail': 'inset 0px 0px 0px 1px #0079b7',
+        'choice-label': 'inset 0px 0px 0px 2px #0079b7',
+      },
+      dropShadow: {
+        '1logo': '0px 1px 11px 5px #000000',
+        '5xl': ['0 35px 35px rgba(0, 0, 0, 0.25)', '0 45px 65px rgba(0, 0, 0, 0.15)'],
+      },
+      animation: {
+        'view-more-bellow-arrow': 'view-more-bellow-arrow 3s infinite',
+      },
+      keyframes: {
+        pulse_tst: {
+          '0%, 20%, 80%, 100%': { transform: 'none', opacity: '0' },
+          '30%, 70%': { transform: 'none', opacity: '1' },
+          '50%': { transform: 'translateY(150%)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 }
