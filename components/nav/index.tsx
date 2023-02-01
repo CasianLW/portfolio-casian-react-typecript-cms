c92fcafab3c26d763c7cbdea522eae006c212c3d
CasianLW <ciorbacasian@gmail.com>
2023-02-01 19:20:54 +0100

    correction navbar: cas ou on clique sur le logo cela enleve la navbar alors qu'avant elle restait immobile

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -83,19 +83,25 @@ const NavComponent: FC = () => {
   return (
     <nav
       className={
-        'fixed inset-0 z-10 grid content-center justify-items-start gap-4 px-[5vw] py-6 before:fixed  before:inset-0 before:bg-cas-black-400 before:transition-all lg:absolute lg:bottom-[initial] lg:flex lg:gap-10 lg:py-8 lg:before:hidden' +
-        `${navIsClosed ? ' absolute before:-translate-x-full before:delay-[700ms]' : ' fixed'}`
+        'fixed inset-0 z-10 grid content-center justify-items-start gap-4 px-[5vw] py-6 before:fixed  before:inset-0 before:bg-cas-black-400 before:transition-all lg:absolute lg:bottom-[initial] lg:flex lg:gap-10 lg:py-8 lg:before:hidden ' +
+        `${navIsClosed ? ' absolute before:translate-x-full before:delay-[500ms] before:rounded-l-[48%]' : ' fixed'}`
       }
       ref={navRef}
     >
-      <Link href="/" className="absolute top-5 left-[5vw] block w-[50px] lg:static lg:my-auto lg:mr-auto lg:w-[64px]">
+      <NavLinkComponent
+        img={logoCasianBlanc}
+        href={'/'}
+        navLink={NavLinkEnum.Home}
+        supplentaryClasses={'absolute top-5 left-[5vw] block w-[50px] lg:static lg:my-auto lg:mr-auto lg:w-[64px]'}
+      />
+      {/* <Link href="/" className="absolute top-5 left-[5vw] block w-[50px] lg:static lg:my-auto lg:mr-auto lg:w-[64px]">
         <Image
           className="max-w-[200%] "
           // src={navIsClosed ? logoCwr : logoCwrMonochrome}
           src={logoCasianBlanc}
           alt="Logo Casian"
         />
-      </Link>
+      </Link> */}
 
       <BurgerComponent />
 
@@ -114,37 +120,30 @@ export default NavComponent
 
 const navLinks = [
   {
-    navLink: NavLinkEnum.About,
+    navLink: NavLinkEnum.Home,
     supplementaryClasses: 'lg:ml-auto delay-[100ms]',
   },
   {
-    navLink: NavLinkEnum.Works,
+    navLink: NavLinkEnum.About,
     supplementaryClasses: 'delay-[200ms]',
   },
   {
-    navLink: NavLinkEnum.Contact,
+    navLink: NavLinkEnum.Works,
     supplementaryClasses: 'delay-[300ms]',
   },
-  //   {
-  //     navLink: NavLinkEnum.Entreprise,
-  //     supplementaryClasses: 'delay-[400ms]',
-  //   },
-  //   {
-  //     navLink: NavLinkEnum.Financement,
-  //     supplementaryClasses: 'delay-[500ms]',
-  //   },
-  //   {
-  //     navLink: NavLinkEnum.Contact,
-  //     supplementaryClasses: 'lg:ml-auto delay-[600ms]',
-  //   },
+  {
+    navLink: NavLinkEnum.Contact,
+    supplementaryClasses: 'lg:ml-auto delay-[400ms]',
+  },
 ]
 
 interface NavLinkComponentProp {
   href: string
   navLink: NavLinkEnum
   supplentaryClasses: string
+  img?: string
 }
-const NavLinkComponent: FC<NavLinkComponentProp> = ({ href, supplentaryClasses, navLink }) => {
+const NavLinkComponent: FC<NavLinkComponentProp> = ({ href, supplentaryClasses, navLink, img }) => {
   const { navIsClosed, setNavIsClosed, activeNavLink } = useNavSettingsContext()
 
   return (
@@ -152,14 +151,14 @@ const NavLinkComponent: FC<NavLinkComponentProp> = ({ href, supplentaryClasses, 
       href={href}
       className={
         'z-0 text-[44px] font-semibold no-underline transition-all lg:translate-x-0 lg:text-[20px]' +
-        ` ${supplentaryClasses} ${navIsClosed ? 'translate-x-[-100vw]' : 'translate-x-0'} 
+        ` ${supplentaryClasses} ${navIsClosed && !img ? 'translate-x-[100vw]' : 'translate-x-0'} 
                     ${activeNavLink === navLink && 'link-active'}`
       }
       //   ${activeNavLink === NavLinkEnum.Contact && 'link-special'}
       //   ${activeNavLink === NavLinkEnum.Works && 'link-special'}
       onClick={() => setNavIsClosed(true)}
     >
-      {navLink}
+      {img ? <Image className="max-w-[200%] " src={img} alt="${img}" /> : navLink}
     </Link>
   )
 }
