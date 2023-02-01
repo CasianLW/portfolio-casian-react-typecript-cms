35d913890b73acf6f2f59abfe810b14b60129b28
CasianLW <ciorbacasian@gmail.com>
2023-02-01 18:17:14 +0100

    Ajout des images et vecteurs ainsi que leurs imports/exports

---
+++ assets/shared/index.ts
@@ -0,0 +1,5 @@
+import logoCasianBlanc from './logo_casian_blanc.svg'
+import profilePicture from './casianpt.png'
+// import ContactProfileBackground from './contact-background-mouvement.mp4'
+
+export { logoCasianBlanc, profilePicture }
