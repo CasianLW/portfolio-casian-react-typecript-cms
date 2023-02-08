c34fd6caa5555ed249a6c1fc737696e770ead9f8
CasianLW <ciorbcasian@gmail.com>
2023-02-09 00:39:21 +0100

    debut dev composant page accueil, animation qui incite au scroll

---
+++ components/homepage/scrollAnimationComponent.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+
+const ScrollAnimationComponent: FC = () => {
+  return (
+    <div className="hero">
+      <div className="scroll-down">
+        <div className="mouse">
+          <span></span>
+        </div>
+      </div>
+
+      <div className="point">
+        <span></span>
+      </div>
+    </div>
+  )
+}
+
+export default ScrollAnimationComponent
