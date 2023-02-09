c3a5b451bbd1caca4d2b9082e93caa572b9cffa8
CasianLW <ciorbacasian@gmail.com>
2023-02-09 14:15:00 +0100

    fin design et structure composant scroll page accueil

--- components/homepage/scrollAnimationComponent.tsx
+++ components/homepage/scrollAnimationComponent.tsx
@@ -2,14 +2,14 @@ import { FC } from 'react'
 
 const ScrollAnimationComponent: FC = () => {
   return (
-    <div className="hero">
-      <div className="scroll-down">
-        <div className="mouse">
+    <div className=" w-[30px] h-[80px] flex fixed bottom-8 left-[5%] items-center flex-col transition-opacity ease-out duration-500">
+      <div className=" ">
+        <div className=" w-[0.12rem] h-10 bg-cas-black-400 rounded-full flex">
           <span></span>
         </div>
       </div>
 
-      <div className="point">
+      <div className=" bg-cas-white-100 w-[7.5px] h-[7.5px] rounded-full translate-y-[-550%]  animate-move-point">
         <span></span>
       </div>
     </div>
