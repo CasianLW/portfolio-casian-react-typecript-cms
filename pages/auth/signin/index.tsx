c1a6333062cdc71413f4c5c30ca90c9fd0e961a9
CasianLW <ciorbacasian@gmail.com>
2023-02-09 15:30:55 +0100

    methode qui set les user/pwd lors du submit, et ses reglages dans le form

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -1,19 +1,44 @@
-const SigninPage = () => {
+import { NextPage } from 'next'
+import { FormEvent, useState } from 'react'
+
+export const SigninPage: NextPage = () => {
+  const [credentials, setCredentials] = useState({ username: '', password: '' })
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(credentials)
+  }
   return (
     <>
       <header className="top-header lateral-space">
         <h1 className="main-title">Espace Admin</h1>
       </header>
-      <form className="mt-40 flex flex-col w-72 m-auto" action="">
+      <form className="mt-40 flex flex-col w-72 m-auto" onSubmit={onSubmit}>
         <h3 className="text-2xl font-bold">Authentification</h3>
         <label className="mt-8" htmlFor="username">
           Username
         </label>
-        <input id="username" required type="text" placeholder="your user name here..." />
+        <input
+          required
+          id="username"
+          name="username"
+          type="text"
+          placeholder="your username here..."
+          value={credentials.username}
+          onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+        />
         <label className="mt-8" htmlFor="password">
           Password
         </label>
-        <input required type="password" placeholder="your password name here..." />
+        <input
+          required
+          id="password"
+          name="password"
+          type="password"
+          placeholder="your password here..."
+          value={credentials.password}
+          onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+        />
         <button className="mt-8">Se connecter</button>
       </form>
     </>
