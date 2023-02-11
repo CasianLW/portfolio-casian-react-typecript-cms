a2556abd8d7fdd393d352f9234a6e64a819df119
CasianLW <ciorbacasian@gmail.com>
2023-02-11 16:39:30 +0100

    import filtre projets page works

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,6 +1,7 @@
 import { Seo } from '@/cas-types'
 import NavComponent, { NavLinkEnum } from '@/components/nav'
 import SeoComponent from '@/components/shared/seo-component'
+import ProjectFilterComponent from '@/components/works/projectFilterComponent'
 import { useNavSettingsContext } from '@/context/nav-settings-context'
 import { getPageSeoBySlug } from '@/utils/content-api'
 import { GetStaticProps, NextPage } from 'next'
@@ -28,6 +29,7 @@ const Works: NextPage<Props> = ({ seo }) => {
       <header className="top-header lateral-space">
         <h1 className="main-title">Works</h1>
       </header>
+      <ProjectFilterComponent />
     </>
   )
 }
