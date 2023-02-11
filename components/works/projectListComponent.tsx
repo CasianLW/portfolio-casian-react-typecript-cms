3da8ff47ec581c10457f7b94fb0ee33dcc873a71
CasianLW <ciorbcasian@gmail.com>
2023-02-11 22:12:50 +0100

    fin structure et avancement sur le design de la liste des projets

--- components/works/projectListComponent.tsx
+++ components/works/projectListComponent.tsx
@@ -1,24 +1,47 @@
-import { FC, useEffect, WheelEvent, TouchEvent, useRef, useState } from 'react'
+import { FC, useEffect, WheelEvent, TouchEvent, useRef, useState, useContext } from 'react'
 import Link from 'next/link'
 
-import { useNavSettingsContext } from '@/context/nav-settings-context'
-
-import { logoCasianBlanc } from '@/assets/shared'
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import { getPathFromNavLink, NavLinkEnum } from '../nav'
-import { homepageMobileApps } from '@/assets/homepage'
-import { GetTransformX } from '@/utils/scroll-touchmove'
 import { workCWRimg } from '@/assets/works'
+import ProjectFilterComponent from './projectFilterComponent'
 
 const ProjectListComponent: FC = () => {
+  //   const filterData = GetFilterData()
+
   return (
-    <section className="bg-cwr-blue-400 pt-8 text-center text-white">
+    <section className="bg-cwr-blue-400 pt-8 text-center w-4/5 m-auto text-white sm:flex sm:flex-wrap ">
       <ProjectComponent
-        classCatagory={'crea all uxui'}
+        classCatagory={'dev all'}
         linkRef={NavLinkEnum.Works}
-        title={''}
-        titleSecondary={''}
-        imageRef={''}
+        title={'CWR'}
+        titleSecondary={'Dev & Design website cwr'}
+        imageRef={workCWRimg}
+        // displayCatagory={}
+      />
+      <ProjectComponent
+        classCatagory={'dev all'}
+        linkRef={NavLinkEnum.Works}
+        title={'CWR'}
+        titleSecondary={'Dev & Design website cwr'}
+        imageRef={workCWRimg}
+        // displayCatagory={}
+      />
+      <ProjectComponent
+        classCatagory={'dev all'}
+        linkRef={NavLinkEnum.Works}
+        title={'CWR'}
+        titleSecondary={'Dev & Design website cwr'}
+        imageRef={workCWRimg}
+        // displayCatagory={}
+      />
+      <ProjectComponent
+        classCatagory={'dev all'}
+        linkRef={NavLinkEnum.Works}
+        title={'CWR'}
+        titleSecondary={'Dev & Design website cwr'}
+        imageRef={workCWRimg}
+        // displayCatagory={}
       />
     </section>
   )
@@ -28,21 +51,33 @@ export default ProjectListComponent
 
 interface ProjectProps {
   classCatagory: string
+  //   displayCatagory: string
   linkRef: NavLinkEnum
   title: string
   titleSecondary: string
-  imageRef: string
+  imageRef: StaticImageData
 }
-const ProjectComponent: FC<ProjectProps> = ({ classCatagory, linkRef, imageRef, title, titleSecondary }) => {
+const ProjectComponent: FC<ProjectProps> = ({
+  classCatagory,
+  linkRef,
+  imageRef,
+  title,
+  titleSecondary,
+  //   displayCatagory,
+}) => {
   return (
-    <div id="cut" className={`${classCatagory} row  `}>
-      <Link className="banner w-fit" href={getPathFromNavLink(linkRef)}>
+    <div
+      id={classCatagory}
+      className={`${classCatagory} relative w-4/5 mt-8 m-auto sm:w-1/2 sm:p-10 lg:w-1/3  `}
+      // style={{ display: displayCatagory }}
+    >
+      <Link className="w-fit" href={getPathFromNavLink(linkRef)}>
         {/* <a className="md:order-1">About</a> */}
-        <Image className="w-[80vw]" src={imageRef} alt={title}></Image>
-        <div className="heading">
-          <h2>{title}</h2>
-          <h3>{titleSecondary}</h3>
+        <div className="absolute ml-[12%] text-left -top-8">
+          <h2 className="text-4xl font-semibold">{title}</h2>
+          <h3 className="text-xs">{titleSecondary}</h3>
         </div>
+        <Image className="w-[80vw]" src={imageRef} alt={title}></Image>
       </Link>
     </div>
   )
