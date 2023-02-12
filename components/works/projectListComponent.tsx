56e0a09e891d0ac0d29e350ec9b6980a14854ec2
CasianLW <ciorbcasian@gmail.com>
2023-02-12 01:12:42 +0100

    MAJ enorme, changement de logique et epuration du composant pour une utilisation plus logique/simplifiée

--- components/works/projectListComponent.tsx
+++ components/works/projectListComponent.tsx
@@ -1,53 +1,9 @@
-import { FC, useEffect, WheelEvent, TouchEvent, useRef, useState, useContext } from 'react'
+import { FC } from 'react'
 import Link from 'next/link'
 
 import Image, { StaticImageData } from 'next/image'
 import { getPathFromNavLink, NavLinkEnum } from '../nav'
-import { workCWRimg } from '@/assets/works'
-import ProjectFilterComponent from './projectFilterComponent'
-
-const ProjectListComponent: FC = () => {
-  //   const filterData = GetFilterData()
-
-  return (
-    <section className="bg-cwr-blue-400 pt-8 text-center w-4/5 m-auto text-white sm:flex sm:flex-wrap ">
-      <ProjectComponent
-        classCatagory={'dev all'}
-        linkRef={NavLinkEnum.Works}
-        title={'CWR'}
-        titleSecondary={'Dev & Design website cwr'}
-        imageRef={workCWRimg}
-        // displayCatagory={}
-      />
-      <ProjectComponent
-        classCatagory={'dev all'}
-        linkRef={NavLinkEnum.Works}
-        title={'CWR'}
-        titleSecondary={'Dev & Design website cwr'}
-        imageRef={workCWRimg}
-        // displayCatagory={}
-      />
-      <ProjectComponent
-        classCatagory={'dev all'}
-        linkRef={NavLinkEnum.Works}
-        title={'CWR'}
-        titleSecondary={'Dev & Design website cwr'}
-        imageRef={workCWRimg}
-        // displayCatagory={}
-      />
-      <ProjectComponent
-        classCatagory={'dev all'}
-        linkRef={NavLinkEnum.Works}
-        title={'CWR'}
-        titleSecondary={'Dev & Design website cwr'}
-        imageRef={workCWRimg}
-        // displayCatagory={}
-      />
-    </section>
-  )
-}
-
-export default ProjectListComponent
+import { motion } from 'framer-motion'
 
 interface ProjectProps {
   classCatagory: string
@@ -55,7 +11,7 @@ interface ProjectProps {
   linkRef: NavLinkEnum
   title: string
   titleSecondary: string
-  imageRef: StaticImageData
+  imageRef: string | StaticImageData
 }
 const ProjectComponent: FC<ProjectProps> = ({
   classCatagory,
@@ -66,19 +22,26 @@ const ProjectComponent: FC<ProjectProps> = ({
   //   displayCatagory,
 }) => {
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      //   transition={{ duration: 0.4 }}
+      layout
       id={classCatagory}
       className={`${classCatagory} relative w-4/5 mt-8 m-auto sm:w-1/2 sm:p-10 lg:w-1/3  `}
       // style={{ display: displayCatagory }}
     >
       <Link className="w-fit" href={getPathFromNavLink(linkRef)}>
         {/* <a className="md:order-1">About</a> */}
-        <div className="absolute ml-[12%] text-left -top-8">
-          <h2 className="text-4xl font-semibold">{title}</h2>
-          <h3 className="text-xs">{titleSecondary}</h3>
+        <div className="absolute ml-[12%] text-left -top-8 sm:-top-1">
+          <h2 className="text-4xl font-semibold line-clamp-1">{title}</h2>
+          <h3 className="text-xs line-clamp-1">{titleSecondary}</h3>
         </div>
-        <Image className="w-[80vw]" src={imageRef} alt={title}></Image>
+        <Image width={500} height={500} className="w-[80vw]" src={imageRef} alt={title}></Image>
       </Link>
-    </div>
+    </motion.div>
   )
 }
+
+export default ProjectComponent
