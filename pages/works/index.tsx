67ce7840c5b1efc3bf8d7b2ae823a8304f3a2d0d
CasianLW <ciorbacasian@gmail.com>
2023-02-12 01:14:28 +0100

    Integration des nouveaux composants page works: filter et liste des projets

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,11 +1,17 @@
+import { workCWRimg } from '@/assets/works'
 import { Seo } from '@/cas-types'
 import NavComponent, { NavLinkEnum } from '@/components/nav'
 import SeoComponent from '@/components/shared/seo-component'
-import ProjectFilterComponent from '@/components/works/projectFilterComponent'
+import FilterComponent from '@/components/works/filterCompoenent'
+import ProjectComponent from '@/components/works/projectListComponent'
+import ProjectListComponent from '@/components/works/projectListComponent'
 import { useNavSettingsContext } from '@/context/nav-settings-context'
 import { getPageSeoBySlug } from '@/utils/content-api'
+import { AnimatePresence, motion } from 'framer-motion'
 import { GetStaticProps, NextPage } from 'next'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+const YOUR_ACCESS_KEY = process.env.YOUR_ACCESS_KEY
 
 interface Props {
   seo: Seo
@@ -23,13 +29,53 @@ const Works: NextPage<Props> = ({ seo }) => {
     setActiveNavLink(NavLinkEnum.Works)
   }, [setActiveNavLink])
 
+  // filter
+  const [projects, setProjects] = useState([])
+  const [filtred, setFiltred] = useState([])
+  const [activeCategory, setActiveCategory] = useState('')
+
+  useEffect(() => {
+    fetchPopular()
+  }, [])
+  const fetchPopular = async () => {
+    const data = await fetch(`https://api.artic.edu/api/v1/artworks`)
+    const projects = await data.json()
+    setProjects(projects.data)
+    setFiltred(projects.data)
+    // console.log(projects.data)
+  }
+
   return (
     <>
       <SeoComponent seo={seo} />
       <header className="top-header lateral-space">
         <h1 className="main-title">Works</h1>
       </header>
-      <ProjectFilterComponent />
+      {/* <ProjectFilterComponent /> */}
+      <FilterComponent
+        projects={projects}
+        setFiltred={setFiltred}
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+      />
+
+      {/* <ProjectListComponent /> */}
+      <motion.section layout className="bg-cwr-blue-400 pt-8 text-center w-4/5 m-auto text-white sm:flex sm:flex-wrap ">
+        <AnimatePresence>
+          {filtred.map((project: never | any) => {
+            return (
+              <ProjectComponent
+                key={project.id}
+                classCatagory={'dev all'}
+                linkRef={NavLinkEnum.Works}
+                title={project.artist_title}
+                titleSecondary={project.title}
+                imageRef={`https://www.artic.edu/iiif/2/${project.image_id}/full/843,/0/default.jpg`}
+              />
+            )
+          })}
+        </AnimatePresence>
+      </motion.section>
     </>
   )
 }
