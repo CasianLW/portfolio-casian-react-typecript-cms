97e34de7faf33246a31f8bfd7e79dd21c3ee77ff
CasianLW <ciorbacasian@gmail.com>
2023-02-13 02:33:33 +0100

    Debut dev partie importation services depuis api/json

--- utils/content-api.ts
+++ utils/content-api.ts
@@ -1,9 +1,10 @@
-import { Project, Seo } from '@/cas-types'
+import { Service, Project, Seo } from '@/cas-types'
 import fs from 'fs'
 import { join } from 'path'
 
 const contentPagesDirectory = join(process.cwd(), '_content/pages')
 const contentProjectsDirectory = join(process.cwd(), '_content/projects')
+const contentServicesDirectory = join(process.cwd(), '_content/services')
 
 export const getPageSeoBySlug: (slug: string) => Seo = (slug) => {
   const realSlug = slug.replace(/\.json$/, '')
@@ -54,7 +55,7 @@ export const getProjectByFileName: (fileName: string) => Project = (fileName) =>
 export const getProjectsSlugs: () => string[] = () =>
   fs.readdirSync(contentProjectsDirectory).map((fileName) => fileName.replace(/\.json$/, ''))
 
-// getting all formatins
+// getting all projects
 export const getAllProjects: () => Project[] = () => {
   // add paths for getting all Projects
   const filesNames = fs.readdirSync(contentProjectsDirectory)
@@ -70,4 +71,49 @@ export const getAllProjects: () => Project[] = () => {
 
   // return the available Projects
   return projects
+}
+
+// services
+
+export const getServiceByFileName: (fileName: string) => Service = (fileName) => {
+  const fullPath = join(contentServicesDirectory, fileName)
+
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  const fileData = JSON.parse(fileContents)
+
+  const { title, liens, primaryImage, secondaryImage, description, seo, order } = fileData
+  const slug = fileName.replace(/\.json$/, '')
+  // const categorys = fileData.categorys.map((s: { category: string }) => s.category)
+  const categories = fileData.categories.map((g: { categorie: string }) => g.categorie)
+
+  // const methods = fileData.methods.map((g: { method: string }) => g.method)
+
+  const service: Service = {
+    title,
+    description,
+    primaryImage,
+    slug,
+    order,
+    categories,
+  }
+
+  return service
+}
+// get all services
+export const getAllServices: () => Service[] = () => {
+  // add paths for getting all Services
+  const filesNames = fs.readdirSync(contentServicesDirectory)
+
+  // get the Projects from the filenames with the needed fields sorted by date in descending order
+  const services = filesNames
+    .map((fileName) => getProjectByFileName(fileName))
+
+    .sort((f1, f2) => {
+      if (f1.order === f2.order) return f1.title.localeCompare(f2.title)
+      return f1.order - f2.order
+    })
+
+  // return the available Projects
+  return services
 }
