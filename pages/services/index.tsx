81cd379b366300cc8f3cc1d07a856f886b83abe0
CasianLW <ciorbacasian@gmail.com>
2023-02-13 02:33:06 +0100

    Systeme de passage  d'une catagrorie de service a une autre ! Importation des composants pannel qui s'affichent en fonction de la categorie choisie Reste a retravailler le design et maper les données

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,11 +1,13 @@
 // import ContectComponent from '@/components/contact-components/formComponent'
 import { Seo } from '@/cas-types'
 import { NavLinkEnum } from '@/components/nav'
+import PanelComponent from '@/components/services/panelComponent'
 import SeoComponent from '@/components/shared/seo-component'
 import { useNavSettingsContext } from '@/context/nav-settings-context'
 import { getPageSeoBySlug } from '@/utils/content-api'
+import { motion } from 'framer-motion'
 import { GetStaticProps, NextPage } from 'next'
-import { useEffect } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 interface Props {
   seo: Seo
@@ -17,7 +19,14 @@ export const getStaticProps: GetStaticProps = async () => {
     props: { seo },
   }
 }
+
 const Services: NextPage<Props> = ({ seo }) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+
+  const handleClick = (index: number) => {
+    setActiveIndex(index)
+  }
+
   const { setActiveNavLink } = useNavSettingsContext()
 
   useEffect(() => {
@@ -30,8 +39,73 @@ const Services: NextPage<Props> = ({ seo }) => {
       <header className="top-header lateral-space">
         <h1 className="main-title">Services</h1>
       </header>
+
+      <div className="max-w-3xl mx-auto px-8 sm:px-0">
+        <div className="sm:w-7/12 sm:mx-auto">
+          <div
+            role="tablist"
+            aria-label="tabs"
+            className="relative w-full mx-auto h-14 grid grid-cols-3 items-center  rounded-full bg-cas-black-400 overflow-hidden shadow-2xl shadow-900/20 transition"
+          >
+            <motion.div
+              layout
+              className={`absolute w-1/3 m-2  top-0 bottom-0 rounded-full bg-white shadow-md ${
+                activeIndex === 0
+                  ? 'left-0 w-[29%]'
+                  : activeIndex === 1
+                  ? 'left-[31%]'
+                  : activeIndex === 2
+                  ? 'right-0 w-[28%]'
+                  : ''
+              }`}
+            ></motion.div>
+            <Tab index={0} activeIndex={activeIndex} handleClick={handleClick}>
+              Complets
+            </Tab>
+            <Tab index={1} activeIndex={activeIndex} handleClick={handleClick}>
+              Spécifiques
+            </Tab>
+            <Tab index={2} activeIndex={activeIndex} handleClick={handleClick}>
+              Autres
+            </Tab>
+          </div>
+          <motion.div layout className="tab-content mt-12">
+            <PanelComponent index={0} activeIndex={activeIndex} tabCategory={'packs'} />
+
+            <PanelComponent index={1} activeIndex={activeIndex} tabCategory={'specific'} />
+
+            <PanelComponent index={2} activeIndex={activeIndex} tabCategory={'autres'} />
+          </motion.div>
+        </div>
+      </div>
     </>
   )
 }
 
 export default Services
+//
+//
+
+interface TabProps {
+  index: number
+  activeIndex: number
+  handleClick: (index: number) => void
+  children: string
+}
+
+export const Tab: FC<TabProps> = ({ index, activeIndex, handleClick, children }) => {
+  return (
+    <motion.button
+      layout
+      role="tab"
+      aria-selected={index === activeIndex}
+      aria-controls={`panel-${index + 1}`}
+      id={`tab-${index + 1}`}
+      tabIndex={index === activeIndex ? 0 : -1}
+      className="relative block h-10 w-full text-center tab rounded-full"
+      onClick={() => handleClick(index)}
+    >
+      <span className={`text-cas-white-100  ${index === activeIndex ? 'text-cas-black-400' : ''}`}>{children}</span>
+    </motion.button>
+  )
+}
