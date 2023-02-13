f141cba5a3d2ef3fb5b40e0984acd8344d17085c
CasianLW <ciorbcasian@gmail.com>
2023-02-13 02:31:29 +0100

    ajout image et ses imports/exports

--- assets/shared/index.ts
+++ assets/shared/index.ts
@@ -1,5 +1,6 @@
 import logoCasianBlanc from './logo_casian_blanc.svg'
 import profilePicture from './casianpt.png'
+import showMoreArrows from './show_more_arrows.svg'
 // import ContactProfileBackground from './contact-background-mouvement.mp4'
 
-export { logoCasianBlanc, profilePicture }
+export { logoCasianBlanc, profilePicture, showMoreArrows }
