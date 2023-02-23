54596f3053af885a04f599e3f8233997ad038238
CasianLW <ciorbacasian@gmail.com>
2023-02-23 14:09:56 +0100

    under dev, test composant addWork

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,4 +1,5 @@
 import { Seo } from '@/cas-types'
+import AddWorkComponent from '@/components/admin/works/addWorkComponent'
 import NavComponent, { NavLinkEnum } from '@/components/nav'
 import SeoComponent from '@/components/shared/seo-component'
 import { useNavSettingsContext } from '@/context/nav-settings-context'
@@ -11,117 +12,160 @@ interface Props {
 }
 
 const AdminPage: NextPage<Props> = () => {
-  const [formData, setFormData] = useState({
-    seoTitle: '',
-    seoDescription: '',
-    title: '',
-    slug: '',
-    description: '',
-    imgLink: '',
-  })
-  const [submitStatus, setSubmitStatus] = useState<'submitting' | 'success' | 'error' | 'idle'>('idle')
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    const response = await fetch('/api/works/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        seo: {
-          title: formData.seoTitle,
-          description: formData.seoDescription,
-        },
-        title: formData.title,
-        slug: formData.slug,
-        description: formData.description,
-        coverImage: formData.imgLink,
-      }),
-    })
-
-    if (response.ok) {
-      // Handle success
-      setSubmitStatus('success')
-      setFormData({
-        seoTitle: '',
-        seoDescription: '',
-        title: '',
-        slug: '',
-        description: '',
-        imgLink: '',
-      })
-      setTimeout(() => {
-        setSubmitStatus('idle')
-      }, 4000)
-    } else {
-      // Handle error
-      setSubmitStatus('error')
-      setTimeout(() => {
-        setSubmitStatus('idle')
-      }, 4000)
-    }
-  }
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    })
-  }
-  const alertComponent = () => {
-    switch (submitStatus) {
-      case 'submitting':
-        return <div className="alert alert-info">Submitting form...</div>
-      case 'success':
-        return <div className="alert alert-success">Form submitted successfully!</div>
-      case 'error':
-        return <div className="alert alert-danger">Form submission failed. Please try again later.</div>
-      default:
-        return null
-    }
-  }
   return (
     <>
       <SeoComponent seo={{ title: 'Admin Dashboard', description: 'Admin dashboard' }} />
       <header className="top-header lateral-space">
-        <h1 className="main-title">AdminPage</h1>
-        <form className="mt-10" onSubmit={handleSubmit}>
-          <h2>Ajouter un projet</h2>
-          <div className="grid">
-            <label htmlFor="title">Title</label>
-            <input required type="text" name="title" value={formData.title} onChange={handleInputChange} />
-          </div>
-          <div className="grid">
-            <label htmlFor="slug">Slug</label>
-            <input required type="text" name="slug" value={formData.slug} onChange={handleInputChange} />
-          </div>
-          <div className="grid">
-            <label htmlFor="description">Description</label>
-            <textarea required name="description" value={formData.description} onChange={handleInputChange} />
-          </div>
-          <div className="grid">
-            <label htmlFor="imgLink">Lien image</label>
-            <input required name="imgLink" value={formData.imgLink} onChange={handleInputChange} />
-          </div>
-          <div className="my-10">
-            <h3>Seo settings:</h3>
-            <div className="grid">
-              <label htmlFor="seoTitle">Titre seo</label>
-              <input required name="seoTitle" value={formData.seoTitle} onChange={handleInputChange} />
-            </div>
-            <div className="grid">
-              <label htmlFor="seoDescription">Description seo</label>
-              <input required name="seoDescription" value={formData.seoDescription} onChange={handleInputChange} />
-            </div>
-          </div>
-          <button type="submit">Enregistrer projet</button>
-        </form>
-        {alertComponent()}
+        <div>
+          <a>Welcome Dashboard</a>
+          <a>General & Homepage</a>
+          <a>Works</a>
+          <a>Services</a>
+        </div>
       </header>
+      <main className="">
+        <section>
+          <div>list of projects</div>
+          <div>
+            <AddWorkComponent />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
+
+//   const [formData, setFormData] = useState({
+//     seoTitle: '',
+//     seoDescription: '',
+//     title: '',
+//     slug: '',
+//     description: '',
+//     imgLink: '',
+//     published: false,
+//   })
+//   const [submitStatus, setSubmitStatus] = useState<'submitting' | 'success' | 'error' | 'idle'>('idle')
+
+//   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault()
+
+//     const response = await fetch('/api/works/', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         seo: {
+//           title: formData.seoTitle,
+//           description: formData.seoDescription,
+//         },
+//         title: formData.title,
+//         slug: formData.slug,
+//         description: formData.description,
+//         coverImage: formData.imgLink,
+//         published: formData.published,
+//       }),
+//     })
+
+//     if (response.ok) {
+//       // Handle success
+//       setSubmitStatus('success')
+//       setFormData({
+//         seoTitle: '',
+//         seoDescription: '',
+//         title: '',
+//         slug: '',
+//         description: '',
+//         imgLink: '',
+//         published: false,
+//       })
+//       setTimeout(() => {
+//         setSubmitStatus('idle')
+//       }, 4000)
+//     } else {
+//       // Handle error
+//       setSubmitStatus('error')
+//       setTimeout(() => {
+//         setSubmitStatus('idle')
+//       }, 4000)
+//     }
+//   }
+
+//   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+//     setFormData({
+//       ...formData,
+//       [event.target.name]: event.target.value,
+//     })
+//   }
+//   const alertComponent = () => {
+//     switch (submitStatus) {
+//       case 'submitting':
+//         return <div className="alert alert-info">Submitting form...</div>
+//       case 'success':
+//         return <div className="alert alert-success">Form submitted successfully!</div>
+//       case 'error':
+//         return <div className="alert alert-danger">Form submission failed. Please try again later.</div>
+//       default:
+//         return null
+//     }
+//   }
+//   return (
+//     <>
+//       <SeoComponent seo={{ title: 'Admin Dashboard', description: 'Admin dashboard' }} />
+//       <header className="top-header lateral-space">
+//         <h1 className="main-title">AdminPage</h1>
+//         <form className="mt-10" onSubmit={handleSubmit}>
+//           <h2>Ajouter un projet</h2>
+//           <div className="grid">
+//             <label htmlFor="title">Title</label>
+//             <input required type="text" name="title" value={formData.title} onChange={handleInputChange} />
+//           </div>
+//           <div className="grid">
+//             <label htmlFor="slug">Slug</label>
+//             <input required type="text" name="slug" value={formData.slug} onChange={handleInputChange} />
+//           </div>
+//           <div className="grid">
+//             <label htmlFor="description">Description</label>
+//             <textarea required name="description" value={formData.description} onChange={handleInputChange} />
+//           </div>
+//           <div className="grid">
+//             <label htmlFor="imgLink">Lien image</label>
+//             <input required name="imgLink" value={formData.imgLink} onChange={handleInputChange} />
+//           </div>
+//           <div className="my-10">
+//             <h3>Seo settings:</h3>
+//             <div className="grid">
+//               <label htmlFor="seoTitle">Titre seo</label>
+//               <input required name="seoTitle" value={formData.seoTitle} onChange={handleInputChange} />
+//             </div>
+//             <div className="grid">
+//               <label htmlFor="seoDescription">Description seo</label>
+//               <input required name="seoDescription" value={formData.seoDescription} onChange={handleInputChange} />
+//             </div>
+//             <div className="grid">
+//               <label htmlFor="published">Published:</label>
+//               <div>
+//                 <input
+//                   type={'checkbox'}
+//                   name="published"
+//                   checked={formData.published}
+//                   onChange={(event) =>
+//                     setFormData({
+//                       ...formData,
+//                       published: event.target.checked,
+//                     })
+//                   }
+//                 />
+//                 <span>{formData.published ? 'Yes' : 'No'}</span>
+//               </div>
+//             </div>
+//           </div>
+//           <button type="submit">Enregistrer projet</button>
+//         </form>
+//         {alertComponent()}
+//       </header>
+//     </>
+//   )
+// }
 
 export default AdminPage
