980e45366f1e44b64d17a514b0bcc7bd106e167c
CasianLW <ciorbcasian@gmail.com>
2023-02-23 09:57:48 +0100

    Partie Backend Works, call api mongodb

---
+++ pages/api/works/[_slug].tsx
@@ -0,0 +1,62 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+// import dbConnect from '@/utils/mongodb/db-connect'
+import { dbConnect } from '@/utils/mongodb/db-connect'
+import WorkModel from '@/utils/mongodb/work.model'
+// import Works from '@/pages/works'
+// import WorkModel from '@/models/work.model'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { _slug },
+    method,
+  } = req
+
+  await dbConnect()
+
+  switch (method) {
+    case 'GET' /* Get a work item by its slug */:
+      try {
+        const work = await WorkModel.findOne({ slug: _slug })
+
+        if (!work) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: work })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    case 'PUT' /* Edit a work item by its slug */:
+      try {
+        const work = await WorkModel.findOneAndUpdate({ slug: _slug }, req.body, {
+          new: true,
+          runValidators: true,
+        })
+        // console.log(work)
+        if (!work) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: work })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    case 'DELETE' /* Delete a work item by its slug */:
+      try {
+        const deletedWork = await WorkModel.deleteOne({ slug: _slug })
+        if (!deletedWork) {
+          return res.status(400).json({ success: false })
+        }
+        res.status(200).json({ success: true, data: {} })
+      } catch (error) {
+        res.status(400).json({ success: false })
+      }
+      break
+
+    default:
+      res.status(400).json({ success: false })
+      break
+  }
+}
