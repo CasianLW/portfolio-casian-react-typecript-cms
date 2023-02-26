0b4cbc0c27d415c5fdb08caa64031601d584112b
CasianLW <ciorbcasian@gmail.com>
2023-02-26 03:55:42 +0100

    passage des methodes du listWork to the main page

--- pages/admin/works/index.tsx
+++ pages/admin/works/index.tsx
@@ -6,7 +6,7 @@ import SeoComponent from '@/components/shared/seo-component'
 import { useAdminNavSettingsContext } from '@/context/admin-nav-settings-context'
 import AdminLayoutComponent from '@/layout/admin'
 import { NextPage } from 'next'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Props {
   seo: Seo
@@ -18,6 +18,60 @@ const WorksCMS: NextPage<Props> = () => {
     setActiveNavLink(AdminNavLinkEnum.Works)
   }, [setActiveNavLink])
 
+  const [loading, setLoading] = useState(true)
+  const [worksList, setWorksList] = useState([])
+  const [submitStatus, setSubmitStatus] = useState<'submitting' | 'success' | 'error' | 'idle'>('idle')
+  const [messageText, setMessageText] = useState()
+  const getWorksList = async () => {
+    console.log('test du transfer')
+    try {
+      setSubmitStatus('submitting')
+      const response = await fetch('/api/works/', {
+        method: 'GET',
+      })
+      const data = await response.json()
+      console.log(data)
+      if (response.ok) {
+        // Handle success
+        setSubmitStatus('success')
+        setWorksList(data.works.reverse())
+        setLoading(false)
+        // console.log(Array.isArray(worksList) + 'ies it is')
+
+        setMessageText(data.message)
+        setTimeout(() => {
+          setSubmitStatus('idle')
+        }, 4000)
+      }
+    } catch (error: any) {
+      // Handle error
+      // console.log(data.message)
+      setSubmitStatus('error')
+      setMessageText(error)
+      setTimeout(() => {
+        setSubmitStatus('idle')
+      }, 4000)
+    }
+  }
+  useEffect(() => {
+    getWorksList()
+  }, [setSubmitStatus, setMessageText, setWorksList])
+
+  const alertComponent = () => {
+    switch (submitStatus) {
+      case 'submitting':
+        return <div className="alert alert-info text-cas-white-100">Loading works ...</div>
+      case 'success':
+        return <div className="alert alert-success text-cas-white-100">Work list successfully imported!</div>
+      case 'error':
+        return <div className="alert alert-danger text-cas-white-100">{messageText}</div>
+      case 'idle':
+        return <div className="alert alert-danger text-cas-white-100"></div>
+      default:
+        return null
+    }
+  }
+
   return (
     <AdminLayoutComponent>
       <>
@@ -25,15 +79,29 @@ const WorksCMS: NextPage<Props> = () => {
         <header>
           <h1 className="main-title">WorksCMS</h1>
         </header>
+        {alertComponent()}
         <main className="admin-content">
-          <section className="grid gap-4 grid-cols-1 md:grid-cols-2">
+          {loading ? ( // Conditional rendering based on the loading state
+            <div>Loading works ...</div>
+          ) : (
+            <section className="grid gap-4 grid-cols-1 md:grid-cols-2">
+              <div className="col-span-1">
+                <ListWorkComponent getWorksList={() => getWorksList()} worksList={worksList} />
+              </div>
+              <div className="col-span-1">
+                <AddWorkComponent />
+              </div>
+            </section>
+          )}
+
+          {/* <section className="grid gap-4 grid-cols-1 md:grid-cols-2">
             <div className="col-span-1">
-              <ListWorkComponent />
+              <ListWorkComponent getWorksList={() => getWorksList()} worksList={worksList} />
             </div>
             <div className="col-span-1">
               <AddWorkComponent />
             </div>
-          </section>
+          </section> */}
         </main>
       </>
     </AdminLayoutComponent>
