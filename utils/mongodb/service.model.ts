765ad5f21c6b8ea39b610753c8b7d92c3c288190
CasianLW <ciorbacasian@gmail.com>
2023-02-27 16:37:57 +0100

    Service model schema

---
+++ utils/mongodb/service.model.ts
@@ -0,0 +1,41 @@
+// Source : https://github.com/vercel/next.js/tree/canary/examples/with-mongodb-mongoose
+
+// Source : https://gist.github.com/rashidmya/2c075330e636134f00ebe85fbb88fed8
+
+import { models, model, Schema } from 'mongoose'
+
+const ServiceSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  coverImage: {
+    type: String,
+    required: true,
+  },
+
+  category: {
+    type: String,
+    required: true,
+  },
+  order: {
+    type: Number,
+    required: true,
+    default: 99,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const ServiceModel = models.Service || model('Service', ServiceSchema)
+
+export default ServiceModel
