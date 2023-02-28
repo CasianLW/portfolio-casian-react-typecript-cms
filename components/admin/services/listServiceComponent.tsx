abad84a48c2b13cfd9a9eec372e1b234e27e66a4
CasianLW <ciorbcasian@gmail.com>
2023-02-28 03:13:12 +0100

    cleaning component from comms

--- components/admin/services/listServiceComponent.tsx
+++ components/admin/services/listServiceComponent.tsx
@@ -9,56 +9,6 @@ interface ListWorkInterface {
   worksList: never[] | []
 }
 const ListWorkComponent: FC<ListWorkInterface> = ({ getWorksList, worksList }) => {
-  // const [worksList, setWorksList] = useState([])
-  // const [submitStatus, setSubmitStatus] = useState<'submitting' | 'success' | 'error' | 'idle'>('idle')
-  // const [messageText, setMessageText] = useState()
-  // const getWorksList = async () => {
-  //   try {
-  //     setSubmitStatus('submitting')
-  //     const response = await fetch('/api/works/', {
-  //       method: 'GET',
-  //     })
-  //     const data = await response.json()
-  //     console.log(data)
-  //     if (response.ok) {
-  //       // Handle success
-  //       setSubmitStatus('success')
-  //       setWorksList(data.works.reverse())
-  //       setMessageText(data.message)
-  //       setTimeout(() => {
-  //         setSubmitStatus('idle')
-  //       }, 4000)
-  //     }
-  //   } catch (error: any) {
-  //     // Handle error
-  //     // console.log(data.message)
-  //     setSubmitStatus('error')
-  //     setMessageText(error)
-  //     setTimeout(() => {
-  //       setSubmitStatus('idle')
-  //     }, 4000)
-  //   }
-  // }
-  // useEffect(() => {
-  //   getWorksList()
-  //   // console.log(worksList)
-  // }, [setSubmitStatus, setMessageText])
-
-  // const alertComponent = () => {
-  //   switch (submitStatus) {
-  //     case 'submitting':
-  //       return <div className="alert alert-info text-cas-white-100">Loading works ...</div>
-  //     case 'success':
-  //       return <div className="alert alert-success text-cas-white-100">Work list successfully imported!</div>
-  //     case 'error':
-  //       return <div className="alert alert-danger text-cas-white-100">{messageText}</div>
-  //     case 'idle':
-  //       return <div className="alert alert-danger text-cas-white-100"></div>
-  //     default:
-  //       return null
-  //   }
-  // }
-
   function getWorkPass() {
     console.log('test reload')
     getWorksList()
@@ -73,8 +23,6 @@ const ListWorkComponent: FC<ListWorkInterface> = ({ getWorksList, worksList }) =
       <div className="mt-10">
         {Array.isArray(worksList) &&
           worksList.map((work: any, i) => {
-            // const { navLink, supplementaryClasses } = link
-            // const href = getPathFromNavLink(navLink)
             return (
               <WorkItem
                 key={i}
@@ -82,14 +30,11 @@ const ListWorkComponent: FC<ListWorkInterface> = ({ getWorksList, worksList }) =
                 title={work.title}
                 id={work._id}
                 slug={work.slug}
-                // getWorksList={() => getWorksList()}
                 getWorksList={getWorksList}
               />
             )
           })}
       </div>
-
-      {/* {alertComponent()} */}
     </>
   )
 }
@@ -105,7 +50,6 @@ interface WorkItemInterface {
 }
 const WorkItem: FC<WorkItemInterface> = ({ getWorksList, checkboxValue, title, id, slug }) => {
   const [activeDelete, setActiveDelete] = useState(false)
-  // const { navIsClosed, setNavIsClosed, activeNavLink } = useAdminNavSettingsContext()
   function editWork(slugEdit: string) {
     console.log(slugEdit)
   }
@@ -121,27 +65,13 @@ const WorkItem: FC<WorkItemInterface> = ({ getWorksList, checkboxValue, title, i
       const response = await fetch(`/api/works/${slug}`, {
         method: 'DELETE',
       })
-      // const data = await response.json()
-      // console.log(data)
+
       if (response.ok) {
         getWorksList()
         setActiveDelete(false)
-
-        // Handle success
-        // setWorksList(data.works)
-        // setMessageText(data.message)
-        // setTimeout(() => {
-        //   setSubmitStatus('idle')
-        // }, 4000)
       }
     } catch (error: any) {
-      // Handle error
       console.log(error)
-      // setSubmitStatus('error')
-      // setMessageText(error)
-      // setTimeout(() => {
-      //   setSubmitStatus('idle')
-      // }, 4000)
     }
   }
 
