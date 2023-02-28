cc04b7eae59bba6c1b5a4f626bb577825b79c3d9
CasianLW <ciorbcasian@gmail.com>
2023-02-28 02:19:42 +0100

    btn filter design styling

--- components/works/filterCompoenent.tsx
+++ components/works/filterCompoenent.tsx
@@ -21,8 +21,6 @@ export const FilterComponent: FC<filterProps> = ({ projects, setFiltred, activeC
     <div className="filter-selector mt-10 w-4/5 m-auto flex justify-between flex-wrap">
       {/* <p>{{ displayProject }}</p> */}
       <FilterBtnComponent
-        projects={[]}
-        setFiltred={setFiltred}
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
         id={'PC-10'}
@@ -30,24 +28,18 @@ export const FilterComponent: FC<filterProps> = ({ projects, setFiltred, activeC
       />
 
       <FilterBtnComponent
-        projects={[]}
-        setFiltred={setFiltred}
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
         id={'PC-831'}
         title={'Development'}
       />
       <FilterBtnComponent
-        projects={[]}
-        setFiltred={setFiltred}
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
         id={'PC-12'}
         title={'UX/UI Design'}
       />
       <FilterBtnComponent
-        projects={[]}
-        setFiltred={setFiltred}
         activeCategory={activeCategory}
         setActiveCategory={setActiveCategory}
         id={'PC-13'}
@@ -63,14 +55,16 @@ export default FilterComponent
 interface btnProps {
   id: string
   title: string
-  projects: never[] | Dispatch<SetStateAction<never[]>> | SetStateAction<never[]>
-  setFiltred: Dispatch<SetStateAction<never[]>>
   activeCategory: Dispatch<SetStateAction<string>> | string | string[]
   setActiveCategory: Dispatch<SetStateAction<string>>
 }
-const FilterBtnComponent: FC<btnProps> = ({ id, title, projects, setFiltred, activeCategory, setActiveCategory }) => {
+const FilterBtnComponent: FC<btnProps> = ({ id, title, activeCategory, setActiveCategory }) => {
   return (
-    <button onClick={() => setActiveCategory(id)} id={id} className={activeCategory === id ? ' btn-filter-on ' : ''}>
+    <button
+      onClick={() => setActiveCategory(id)}
+      id={id}
+      className={'px-5 py-2 hover:bg-cas-black-500 font-medium' + `${activeCategory === id ? ' btn-filter-on ' : ''}`}
+    >
       {title}
     </button>
   )
