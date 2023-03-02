21b341ed9bd6f58ef994c45816358a1757f80fa7
CasianLW <ciorbcasian@gmail.com>
2023-03-02 21:32:15 +0100

    dev edit page, inspired by works edit structure

---
+++ components/admin/services/editServiceComponent.tsx
@@ -0,0 +1,247 @@
+import { IWorkInfo } from '@/pages/admin/works/[id]'
+import { FC, useState } from 'react'
+
+interface EditServiceInterface {
+  editServiceMethod: () => void
+  dataService: IServiceInfo
+}
+const EditServiceComponent: FC<EditServiceInterface> = ({ dataService, editServiceMethod }) => {
+  const [formData, setFormData] = useState({
+    seoTitle: dataService.seo.title,
+    seoDescription: dataService.seo.description,
+    title: dataService.title,
+    slug: dataService.slug,
+    description: dataService.description,
+    imgLink: dataService.coverImage,
+    secondaryImage: dataService.secondaryImage,
+    category: {
+      dev: dataService.category.dev,
+      uxui: dataService.category.uxui,
+      graphic: dataService.category.graphic,
+    },
+    published: dataService.published,
+  })
+  const [submitStatus, setSubmitStatus] = useState<'submitting' | 'success' | 'error' | 'idle' | 'changed'>('idle')
+  const [messageText, setMessageText] = useState()
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    setSubmitStatus('submitting')
+    const response = await fetch(`/api/services/${dataService._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        seo: {
+          title: formData.seoTitle,
+          description: formData.seoDescription,
+        },
+        title: formData.title,
+        slug: formData.slug,
+        description: formData.description,
+        coverImage: formData.imgLink,
+        category: {
+          dev: formData.category.dev,
+          uxui: formData.category.uxui,
+          graphic: formData.category.graphic,
+        },
+
+        secondaryImage: formData.secondaryImage,
+        published: formData.published,
+      }),
+    })
+    const data = await response.json()
+
+    if (response.ok) {
+      // Handle success
+      setSubmitStatus('success')
+      setMessageText(data.message)
+      editServiceMethod()
+      // setFormData({
+      //   seoTitle: '',
+      //   seoDescription: '',
+      //   title: '',
+      //   slug: '',
+      //   description: '',
+      //   imgLink: '',
+      //   secondaryImage: '',
+      //   category: {
+      //     dev: false,
+      //     uxui: false,
+      //     graphic: false,
+      //   },
+      //   published: false,
+      // })
+      setTimeout(() => {
+        setSubmitStatus('idle')
+      }, 4000)
+    } else {
+      // Handle error
+      // console.log(data.message)
+
+      setSubmitStatus('error')
+      setMessageText(data.message)
+      setTimeout(() => {
+        setSubmitStatus('idle')
+      }, 4000)
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setSubmitStatus('changed')
+
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    })
+  }
+  const alertComponent = () => {
+    switch (submitStatus) {
+      case 'submitting':
+        return <div className="alert alert-info text-cas-white-100">Submitting form...</div>
+      case 'success':
+        return <div className="alert alert-success text-cas-white-100">Form submitted successfully!</div>
+      case 'error':
+        return <div className="alert alert-danger text-cas-white-100">{messageText}</div>
+      case 'idle':
+        return <div className="alert alert-danger text-cas-white-100"></div>
+      default:
+        return null
+    }
+  }
+  return (
+    <>
+      {/* <style>{`
+        form input, form textarea {
+          background-color: red;
+        }
+      `}</style> */}
+      <h2 className="secondary-title">Edit Service</h2>
+      <form className="mt-10 " onSubmit={handleSubmit}>
+        {/* <h3>Ajouter un projet</h3> */}
+        <div className="grid">
+          <label htmlFor="title">Title</label>
+          <input required type="text" name="title" value={formData.title} onChange={handleInputChange} />
+        </div>
+        <div className="grid">
+          <label htmlFor="slug">Slug</label>
+          <input required type="text" name="slug" value={formData.slug} onChange={handleInputChange} />
+        </div>
+        <div className="grid">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="text-cas-black-600"
+            required
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="grid">
+          <label htmlFor="imgLink">Lien image</label>
+          <input required name="imgLink" value={formData.imgLink} onChange={handleInputChange} />
+        </div>
+
+        <div className="grid">
+          <label htmlFor="secondaryImage">Secondary Image</label>
+          <input required name="secondaryImage" value={formData.secondaryImage} onChange={handleInputChange} />
+        </div>
+
+        <div className="grid">
+          <label>Category</label>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                name="dev"
+                checked={formData.category.dev}
+                onChange={() =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    category: { ...prev.category, dev: !prev.category.dev },
+                  }))
+                }
+              />
+              Dev
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="uxui"
+                checked={formData.category.uxui}
+                onChange={() =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    category: { ...prev.category, uxui: !prev.category.uxui },
+                  }))
+                }
+              />
+              UX/UI
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="graphic"
+                checked={formData.category.graphic}
+                onChange={() =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    category: { ...prev.category, graphic: !prev.category.graphic },
+                  }))
+                }
+              />
+              Graphic
+            </label>
+          </div>
+        </div>
+
+        <div className="my-10">
+          <h3>Seo settings:</h3>
+          <div className="grid">
+            <label htmlFor="seoTitle">Titre seo</label>
+            <input required name="seoTitle" value={formData.seoTitle} onChange={handleInputChange} />
+          </div>
+          <div className="grid">
+            <label htmlFor="seoDescription">Description seo</label>
+            <input required name="seoDescription" value={formData.seoDescription} onChange={handleInputChange} />
+          </div>
+          <div className="grid">
+            <label htmlFor="published">Published:</label>
+            <div>
+              <input
+                type={'checkbox'}
+                name="published"
+                checked={formData.published}
+                onChange={(event) =>
+                  setFormData({
+                    ...formData,
+                    published: event.target.checked,
+                  })
+                }
+              />
+              <span className="text-cas-white-100">{formData.published ? 'Yes' : 'No'}</span>
+            </div>
+          </div>
+        </div>
+        <button
+          className={
+            'text-cas-white-100 ' + submitStatus === 'idle'
+              ? ' bg-opacity-50'
+              : submitStatus === 'submitting'
+              ? ' bg-opacity-100 bg-cas-gradient-purple'
+              : submitStatus === 'changed'
+              ? ' bg-opacity-100 bg-cas-white-100'
+              : ''
+          }
+          type="submit"
+        >
+          Update project
+        </button>
+      </form>
+      {alertComponent()}
+    </>
+  )
+}
+
+export default EditServiceComponent
