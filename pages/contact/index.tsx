4146a061d2f18d3e3284a919a836fac8a34472e4
CasianLW <ciorbcasian@gmail.com>
2023-03-07 03:55:35 +0100

    added isLargeScreen use effect and some elements positions on screen width

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -8,7 +8,7 @@ import { getPageSeoBySlug } from '@/utils/content-api'
 import { GetStaticProps, NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 interface Props {
   seo: Seo
@@ -21,10 +21,18 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 const Contact: NextPage<Props> = ({ seo }) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(false)
+
   const { setActiveNavLink } = useNavSettingsContext()
   useEffect(() => {
     setActiveNavLink(NavLinkEnum.Contact)
-  }, [setActiveNavLink])
+    function handleResize() {
+      setIsLargeScreen(window.innerWidth > 640)
+    }
+    handleResize()
+    // Add event listener to resize
+    window.addEventListener('resize', handleResize)
+  }, [setActiveNavLink, isLargeScreen])
 
   return (
     <>
@@ -36,8 +44,8 @@ const Contact: NextPage<Props> = ({ seo }) => {
             Hésitez pas a nous contacter pour toute question, notre formulaire est la pour ca !
           </p>
 
-          {typeof window !== 'undefined' && window.innerWidth > 640 && (
-            <div>
+          <div>
+            {isLargeScreen && (
               <div className="grid grid-cols-2 sm:grid-cols-1">
                 <div className="grid">
                   <a className="py-3" href="tel:+">
@@ -74,19 +82,20 @@ const Contact: NextPage<Props> = ({ seo }) => {
                   </Link>
                 </div>
               </div>
-              <Image
-                src={profilePicture}
-                className="max-w-[50%] m-auto mt-10"
-                alt="Casian Ciorba Image UX UI Designer et Fullstack Mobile Developer"
-                width={500}
-                height={500}
-                // blurDataURL="data:..." automatically provided
-                placeholder="blur" // Optional blur-up while loading
-              />
-            </div>
-          )}
+            )}
+
+            <Image
+              src={profilePicture}
+              className="max-w-[50%] m-auto mt-10"
+              alt="Casian Ciorba Image UX UI Designer et Fullstack Mobile Developer"
+              width={500}
+              height={500}
+              // blurDataURL="data:..." automatically provided
+              placeholder="blur" // Optional blur-up while loading
+            />
+          </div>
         </section>
-        <section className=" mx-[-5vw] sm:mx-0 -mt-20 sm:mt-12 bg-cas-white-100 text-cas-black-600 p-10 rounded-[32px]">
+        <section className="mb-10 sm:mb-0 mx-[-5vw] sm:mx-0 -mt-20 sm:mt-12 bg-cas-white-100 text-cas-black-600 p-10 rounded-[32px]">
           <div>
             <h2 className="text-xl sm:text-3xl font-bold mb-5">
               Vous avez des idées, un projet? J&apos;ai les compétences, travaillons ensemble.
@@ -95,6 +104,44 @@ const Contact: NextPage<Props> = ({ seo }) => {
             <ContectComponent />
           </div>
         </section>
+        {!isLargeScreen && (
+          <div className="grid grid-cols-2 sm:grid-cols-1">
+            <div className="grid">
+              <a className="py-3" href="tel:+">
+                <u>Tel.+33 (0)7 83 41 57 55</u>
+              </a>
+              <a className="py-3" href="mailto:">
+                <u>contact@casian.fr</u>
+              </a>
+            </div>
+            <div className="flex my-auto gap-8 justify-center sm:justify-start">
+              <Link className="w-fit" target="_blank" href={'https://www.linkedin.com/in/casianc/'}>
+                <Image
+                  src={linkedinLogo}
+                  className=" m-auto"
+                  alt="Logo linkedIn"
+                  width={44}
+                  height={44}
+                  // blurDataURL="data:..." automatically provided
+                />
+                <p className="text-sm">LinkedIn</p>
+              </Link>
+              <Link className="w-fit" target="_blank" href={'https://github.com/CasianLW'}>
+                <div className="w-11 h-11 rounded-xl bg-cas-white-100 flex justify-center">
+                  <Image
+                    src={githubLogo}
+                    className="m-auto "
+                    alt="Logo linkedIn"
+                    width={36}
+                    height={36}
+                    // blurDataURL="data:..." automatically provided
+                  />
+                </div>
+                <p className="text-sm">GitHub</p>
+              </Link>
+            </div>
+          </div>
+        )}
       </header>
     </>
   )
