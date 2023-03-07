fff7266cf57d9494c516d1fb4bded7386b09112b
CasianLW <ciorbacasian@gmail.com>
2023-03-07 03:49:33 +0100

    imported linkedin & github logos

--- assets/shared/index.ts
+++ assets/shared/index.ts
@@ -1,6 +1,8 @@
 import logoCasianBlanc from './logo_casian_blanc.svg'
 import profilePicture from './casianpt.png'
 import showMoreArrows from './show_more_arrows.svg'
+import linkedinLogo from './linkedin-logo.svg'
+import githubLogo from './github-logo.svg'
 // import ContactProfileBackground from './contact-background-mouvement.mp4'
 
-export { logoCasianBlanc, profilePicture, showMoreArrows }
+export { logoCasianBlanc, profilePicture, showMoreArrows, linkedinLogo, githubLogo }
