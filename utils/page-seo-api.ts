d356038c7a3c6b25d797eb1f4c31bee392297698
CasianLW <ciorbcasian@gmail.com>
2023-03-29 00:02:39 +0200

    utils recuperer seo pour getstaticprops

---
+++ utils/page-seo-api.ts
@@ -0,0 +1,43 @@
+import { Seo } from '@/cas-types'
+import PageModel from '@/utils/mongodb/page.model'
+
+interface PageData {
+  seo: {
+    title: string
+    description: string
+    searchImage: string
+  }
+  title: string
+}
+export const getPageSeoBySlug = async (slug: string): Promise<Seo> => {
+  try {
+    const response = await fetch(`${process.env.URL}/api/pages/`, {
+      method: 'GET',
+    })
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch SEO data')
+    }
+
+    const data = await response.json()
+
+    const seo = data.pages.find((page: PageData) => page.title === slug)?.seo
+
+    if (seo) {
+      return seo
+    } else {
+      // Return default values if no SEO data is found for the given slug
+      return {
+        title: 'My Website',
+        description: 'Welcome to My Website.',
+      }
+    }
+  } catch (error) {
+    console.error(error)
+    // Return default values in case of an error
+    return {
+      title: 'My Website',
+      description: 'Welcome to My Website.',
+    }
+  }
+}
