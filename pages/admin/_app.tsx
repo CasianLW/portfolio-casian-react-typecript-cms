d3b63ec7f2e106a20e6c347f04d8f550b1c38d7a
CasianLW <ciorbacasian@gmail.com>
2023-03-30 17:33:31 +0200

    Undefined session removed

--- pages/admin/_app.tsx
+++ pages/admin/_app.tsx
@@ -2,11 +2,11 @@ import type { AppProps } from 'next/app'
 
 import AdminLayoutComponent from '@/layout/admin'
 
-const AdminApp = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const AdminApp = ({ Component, pageProps: { ...pageProps } }: AppProps) => {
   return (
     <>
       <div className="top-10">
-        <AdminLayoutComponent session={session}>
+        <AdminLayoutComponent>
           <Component {...pageProps} />
         </AdminLayoutComponent>
       </div>
