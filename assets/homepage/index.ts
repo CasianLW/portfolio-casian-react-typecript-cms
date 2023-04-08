dd4cb10adf385a731bba003aaccb385bfc894351
CasianLW <ciorbacasian@gmail.com>
2023-04-09 01:54:33 +0200

    added youWorkHere asset for homepage

--- assets/homepage/index.ts
+++ assets/homepage/index.ts
@@ -1,5 +1,6 @@
 import homepageMobileApps from './phone_apps.png'
+import yourWorkHere from './your_work.png'
 
 // import ContactProfileBackground from './contact-background-mouvement.mp4'
 
-export { homepageMobileApps }
+export { homepageMobileApps, yourWorkHere }
