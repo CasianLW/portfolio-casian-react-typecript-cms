d356b93f3e0739785e85e6dbab758e79814268bc
CasianLW <ciorbcasian@gmail.com>
2023-04-09 01:56:07 +0200

    Slider scroll limit (min/max), hovers (voir plus), new component for last item on slider

--- components/homepage/sliderComponent.tsx
+++ components/homepage/sliderComponent.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link'
 
 import Image from 'next/image'
 import { getPathFromNavLink, NavLinkEnum } from '../nav'
-import { homepageMobileApps } from '@/assets/homepage'
+import { homepageMobileApps, yourWorkHere } from '@/assets/homepage'
 import { GetTransformX } from '@/utils/scroll-touchmove'
+import React from 'react'
 
 const SliderComponent: FC = () => {
   return (
@@ -17,6 +18,9 @@ const SliderComponent: FC = () => {
           <SliderItemComponent />
           <SliderItemComponent />
           <SliderItemComponent />
+          <SliderItemComponent />
+          <SliderItemComponent />
+          <SliderNextWorkComponent />
         </div>
       </div>
     </section>
@@ -26,17 +30,52 @@ const SliderComponent: FC = () => {
 export default SliderComponent
 
 const SliderItemComponent: FC = () => {
-  const translateX = GetTransformX(-200)
+  // const translateX = GetTransformX(-200)
+  const maxTranslateX = 0 // set your maximum value here
+  const minTranslateX = -1670 // set your negative minimum value here
+  const translateX = Math.min(Math.max(GetTransformX(-200), minTranslateX), maxTranslateX)
   return (
-    <div style={{ transform: `translateX(${translateX}px)` }} className="slider-item flex-1 overflow-hidden w-[50px] ">
+    <div
+      style={{ transform: `translateX(${translateX}px)` }}
+      className="slider-item flex-1 overflow-hidden w-[50px] min-w-[310px] h-fit  sm:px-2"
+    >
       <div className="relative left-[1%] top-[2.5%] w-[98%] h-[95%] flex">
-        <Link className="w-fit" href={getPathFromNavLink(NavLinkEnum.Works)}>
-          {/* <a className="md:order-1">About</a> */}
+        <Link className="w-fit grid homepage-work-link" href={getPathFromNavLink(NavLinkEnum.Works)}>
+          <div className="stack-item z-10 mb-2 self-end translate-y-10 voir-plus-work transition-transform">
+            <p>Voir plus</p>
+          </div>
           <Image
             priority={true}
-            className="w-[80vw] rounded-[32px]"
+            className="w-[80vw] rounded-[32px] stack-item"
             src={homepageMobileApps}
             alt="image applications mobiles casian.fr"
+          ></Image>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+const SliderNextWorkComponent: FC = () => {
+  // const translateX = GetTransformX(-200)
+  const maxTranslateX = 0 // set your maximum value here
+  const minTranslateX = -1670 // set your negative minimum value here
+  const translateX = Math.min(Math.max(GetTransformX(-200), minTranslateX), maxTranslateX)
+  return (
+    <div
+      style={{ transform: `translateX(${translateX}px)` }}
+      className="slider-item flex-1 overflow-hidden w-[50px] min-w-[310px] h-fit "
+    >
+      <div className="relative left-[1%] top-[2.5%] w-[98%] flex flex-col h-full">
+        <Link className="w-fit grid homepage-card-animation" href={getPathFromNavLink(NavLinkEnum.Contact)}>
+          <div className="stack-item self-center">
+            <h3 className=" ml-auto text-xl mr-2 text-right">Your work next?</h3>
+          </div>
+          <Image
+            priority={true}
+            className="w-[80vw] rounded-[32px] stack-item"
+            src={yourWorkHere}
+            alt={'image your next work here?'}
           ></Image>
         </Link>
       </div>
