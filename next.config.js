156383d259e91c14f605be461462916c2413bfd2
CasianLW <ciorbcasian@gmail.com>
2023-04-13 14:34:45 +0200

    Webpack Bundle Analyzer install

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['www.artic.edu', 'images.unsplash.com', 'res.cloudinary.com', 'www.casian.fr'],
+//   },
+// }
+
+// module.exports = nextConfig
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,4 +15,8 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = Object.assign({}, nextConfig, withBundleAnalyzer)
