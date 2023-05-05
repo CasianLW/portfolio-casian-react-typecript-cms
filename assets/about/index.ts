f9551c9c1f57525e7acb6bcb090aad1cb256e05c
CasianLW <ciorbacasian@gmail.com>
2023-05-05 16:51:42 +0200

    added assets for about page

---
+++ assets/about/index.ts
@@ -0,0 +1,5 @@
+import AboutRNCP from './Logo-Titre-RNCP.png'
+
+// import ContactProfileBackground from './contact-background-mouvement.mp4'
+
+export { AboutRNCP }
--- assets/shared/index.ts
+++ assets/shared/index.ts
@@ -1,8 +1,9 @@
 import logoCasianBlanc from './logo_casian_blanc.svg'
 import profilePicture from './casianpt.webp'
+import casianPicture from './casian_profil.webp'
 import showMoreArrows from './show_more_arrows.svg'
 import linkedinLogo from './linkedin-logo.svg'
 import githubLogo from './github-logo.svg'
 // import ContactProfileBackground from './contact-background-mouvement.mp4'
 
-export { logoCasianBlanc, profilePicture, showMoreArrows, linkedinLogo, githubLogo }
+export { logoCasianBlanc, profilePicture, showMoreArrows, linkedinLogo, githubLogo, casianPicture }
