3d0671c7061571808f1b5d1ff3c155b75c14249d
CasianLW <ciorbacasian@gmail.com>
2023-05-05 17:41:51 +0200

    seo prority image

--- pages/works/[slug].tsx
+++ pages/works/[slug].tsx
@@ -87,7 +87,7 @@ const SingleWorkPage: NextPage<Props> = ({ seo, work }) => {
         <header className="top-header lateral-space">
           <h1 className="main-title">{work.title}</h1>
           <div className="grid my-5 sm:grid-cols-2">
-            <CldImage width="600" height="600" src={work.coverImage} alt={`${work.title} work image`} />
+            <CldImage width="600" height="600" priority src={work.coverImage} alt={`${work.title} work image`} />
             <div>
               <h2 className="pt-3 sm:pt-0 mb-3 text-2xl font-semibold">{work.secondaryTitle}</h2>
               <p className="sm:w-4/5">{work.description}</p>
