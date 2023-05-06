4349a6c531475e37503268c87781737a96c952ad
CasianLW <ciorbacasian@gmail.com>
2023-05-06 03:44:20 +0200

    [I]- Get works & selectedOnes + sorting only the selected ones, passing to child

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import { Seo } from '@/cas-types'
 import { GetStaticProps, NextPage } from 'next'
 import { NavLinkEnum } from '@/components/nav'
 import { useNavSettingsContext } from '@/context/nav-settings-context'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import SliderComponent from '@/components/homepage/sliderComponent'
 import TypingTextComponent from '@/components/homepage/typetextComponent'
 import BlobComponent from '@/components/homepage/blobComponent'
 import TextSpinnerComponent from '@/components/homepage/spinnerLinkComponent'
 import ScrollAnimationComponent from '@/components/homepage/scrollAnimationComponent'
 import { getPageSeoBySlug } from '@/utils/page-seo-api'
+import { IWorkInfo } from './admin/works/[id]'
 
 interface Props {
   seo: Seo
@@ -24,9 +25,62 @@ export const getStaticProps: GetStaticProps = async () => {
 }
 const Home: NextPage<Props> = ({ seo }) => {
   const { setActiveNavLink } = useNavSettingsContext()
+
+  const [actualWorksList, setActualWorksList] = useState<string[]>([])
+  const [worksList, setWorksList] = useState<IWorkInfo[]>([])
+  const [sortedWorks, setSortedWorks] = useState<IWorkInfo[]>([])
+
+  const getWorksList = async () => {
+    // console.log('test du transfer')
+    try {
+      const response = await fetch('/api/works/', {
+        method: 'GET',
+      })
+      const responseHp = await fetch('/api/homepage/', {
+        method: 'GET',
+      })
+      const dataHp = await responseHp.json()
+      const data = await response.json()
+      // console.log(data)
+      if (response.ok && responseHp.ok) {
+        // Handle success
+        setWorksList(data.works.reverse())
+        setActualWorksList(dataHp.homepage[0]?.selectedWorks || [])
+        // console.log(dataHp.homepage)
+        // console.log(Array.isArray(worksList) + 'ies it is')
+      }
+    } catch (error: any) {
+      // Handle error
+      // console.log(data.message)
+    }
+  }
+  // getWorksList()
+
+  const sortWorks = (worksList: IWorkInfo[], actualWorksList: string[]): IWorkInfo[] => {
+    return worksList.sort((b, a) => {
+      const aIndex = actualWorksList.indexOf(a.slug)
+      const bIndex = actualWorksList.indexOf(b.slug)
+
+      if (aIndex === -1) return 1
+      if (bIndex === -1) return -1
+
+      return aIndex - bIndex
+    })
+  }
+
   useEffect(() => {
-    setActiveNavLink(NavLinkEnum.Home)
+    const fetchAndSortWorks = async () => {
+      setActiveNavLink(NavLinkEnum.Home)
+      await getWorksList()
+    }
+
+    fetchAndSortWorks()
   }, [setActiveNavLink])
+
+  useEffect(() => {
+    const sorted = sortWorks(worksList, actualWorksList)
+    setSortedWorks(sorted)
+  }, [worksList, actualWorksList])
 
   return (
     <>
@@ -36,7 +90,7 @@ const Home: NextPage<Props> = ({ seo }) => {
         <h1 className="main-title">Freelancer</h1>
         <TypingTextComponent />
         <BlobComponent />
-        <SliderComponent />
+        <SliderComponent projectsList={sortedWorks} />
         <TextSpinnerComponent />
         <ScrollAnimationComponent />
       </header>
